b5110ae528f549032240fe4e0235716620f02b1b
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-22 23:53:58 +0500

    Arrays Basic: Recap - Pretty Print Array

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use std::io;
 use std::convert::TryInto;
+use std::io;
 
 fn main() {
     let mut input_str_arr = String::new();
-    let mut input_index = String::new();
-    let mut new_element = String::new();
     io::stdin().read_line(&mut input_str_arr).unwrap();
-    io::stdin().read_line(&mut input_index).unwrap();
-    io::stdin().read_line(&mut new_element).unwrap();
+    let trimmed = input_str_arr.trim();
+    let arr: [String; 5] = trimmed
+        .split(',')
+        .map(String::from)
+        .collect::<Vec<String>>()
+        .try_into()
+        .unwrap();
 
-    let new_element = new_element.trim().to_string();
-    let index: usize = input_index.trim().parse().unwrap();
-    let mut arr: [String; 5] = input_str_arr.split(',').map(String::from).collect::<Vec<String>>().try_into().unwrap();
-    
+    print!("[");
     // Write your code below
-    // Use arr, index and new_element to solve the challenge    
-    arr[index] = new_element;
     for i in 0..arr.len() {
-        print!("{} ", arr[i]);
+        if i == arr.len() - 1 {
+            print!("{}", arr[i]);
+        } else {
+            print!("{}, ", arr[i]);
+        }
     }
+
+    println!("]");
 }
