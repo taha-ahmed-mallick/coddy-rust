a90505dccf1f11c63298c19ce759f057c9e03e19
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-22 23:20:39 +0500

    Functions: Recap - Sigma Function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use std::io;
 
-fn sigma(n: i32) -> i32 {
+fn is_valid(username: String, password: String) -> bool {
     // Write your code below
-    return n * (n + 1) / 2;
+    if username == "admin" {
+        return true;
+    } else if username == "user" {
+        if password == "qweasd" {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let n: i32 = input.trim().parse().unwrap();
-    let res = sigma(n);
+    let mut input_user = String::new();
+    let mut input_pass = String::new();
+    io::stdin().read_line(&mut input_user).unwrap();
+    io::stdin().read_line(&mut input_pass).unwrap();
+
+    let user: String = input_user.trim().parse().unwrap();
+    let pass: String = input_pass.trim().parse().unwrap();
+    let res = is_valid(user, pass);
     println!("{}", res);
 }
