a0421687efcc81e4fdc58e5ecff457d3409d26f5
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-22 23:28:41 +0500

    Arrays Basic: Declaring Arrays

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
-use std::io;
-
-fn is_valid(username: String, password: String) -> bool {
-    // Write your code below
-    if username == "admin" {
-        return true;
-    } else if username == "user" {
-        if password == "qweasd" {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
-}
-//nothing
 fn main() {
-    let mut input_user = String::new();
-    let mut input_pass = String::new();
-    io::stdin().read_line(&mut input_user).unwrap();
-    io::stdin().read_line(&mut input_pass).unwrap();
-
-    let user: String = input_user.trim().parse().unwrap();
-    let pass: String = input_pass.trim().parse().unwrap();
-    let res = is_valid(user, pass);
-    println!("{}", res);
+    // Create the shopping_list array here
+    let shopping_list= ["bread", "eggs", "milk", "butter"];
+    
+    // Don't change the code below
+    println!("Shopping List:");
+    for i in 0..shopping_list.len() {
+        println!("{}", shopping_list[i]);
+    }
 }
