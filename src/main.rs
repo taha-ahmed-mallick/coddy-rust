02403b6c3a385f81091ba1e9a9f581aeb3a95a86
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-22 22:53:25 +0500

    Functions: Return Value

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 use std::io;
 
-// Function declaration
-fn product (a: i32, b: i32) {
-    println!("{}", a*b);
+fn bigger(arg1: f64, arg2: f64) -> f64 {
+    // Complete the function
+    if arg1 > arg2 {
+        return arg1;
+    } else {
+        return arg2;
+    }
 }
 
 fn main() {
-    let mut input_a = String::new();
-    let mut input_b = String::new();
+    let mut input_iter = String::new();
+    let mut input_num1 = String::new();
+    let mut input_num2 = String::new();
 
-    io::stdin().read_line(&mut input_a).unwrap();
-    io::stdin().read_line(&mut input_b).unwrap();
+    io::stdin().read_line(&mut input_iter).unwrap();
+    io::stdin().read_line(&mut input_num1).unwrap();
+    io::stdin().read_line(&mut input_num2).unwrap();
 
-    let a: i32 = input_a.trim().parse().unwrap();
-    let b: i32 = input_b.trim().parse().unwrap();
-    // Call the function with a and b as arguments
-    product(a, b);
+    let iter: i32 = input_iter.trim().parse().unwrap();
+    let mut num1: f64 = input_num1.trim().parse().unwrap();
+    let mut num2: f64 = input_num2.trim().parse().unwrap();
+
+    for _ in 0..iter {
+        // Write your code below
+        let mut ret: f64 = bigger(num1, num2);
+        if ret == num1 {
+            ret /= 2.0;
+            num1 = ret;
+        } else {
+            ret /= 2.0;
+            num2 = ret;
+        }
+        println!("{}", ret);
+        if ret <= 2.0 {
+            break;
+        }
+    }
 }
