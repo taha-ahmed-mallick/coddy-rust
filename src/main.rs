bf7b92b544e3c1b9213a0452590a6c6c5ab0d4c4
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-22 23:05:22 +0500

    Functions: Recap - Sigma Function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-fn get_user_info() -> (String, i32, String) {
-    // Create name, age, and email variables here
-    let name: String = String::from("Bob");
-    let age: i32 = 25;
-    let email: String = "bob@example.com".to_string();
-    // Return the tuple
-    return (name, age, email);
+use std::io;
+
+fn sigma(n: i32) -> i32 {
+    // Write your code below
+    return n * (n + 1) / 2;
 }
 
 fn main() {
-    // Call get_user_info and destructure the returned tuple
-    let (name, age, email) = get_user_info();
-    
-    // Print the values
-    println!("Name: {}, Age: {}, Email: {}", name, age, email);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: i32 = input.trim().parse().unwrap();
+    let res = sigma(n);
+    println!("{}", res);
 }
