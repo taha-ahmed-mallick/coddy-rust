7f01b24de4b82cb4013b48c07f27f798e14c53e8
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-23 15:39:48 +0500

    Arrays Continued: Better Print

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let numbers = [1, 2, 3, 4, 5];
-println!("{:?} ", numbers);
-let arr = [1.12345, 2.23456, 3.34567];
-println!("{:.2?}", arr);
+    println!("{:?} ", numbers);
+    let arr = [1.12345, 2.23456, 3.34567];
+    println!("{:.2?}", arr);
+    let grades = ['A', 'B', 'C', 'D', 'F'];
+    println!("{:?}", grades);
 }
