ac0317cdb6b27641878bdec344004c5d30df425f
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-23 15:11:35 +0500

    Arrays Continued: Array Methods

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-fn process_array(arr: &mut [i32]) {
-	arr[0] = 3;
+use std::io;
+use std::convert::TryInto;
+
+fn analyze_array(arr: &mut [String; 5]) {
+    // Write code here
+    println!("Is empty: {}", arr.is_empty());
+    println!("Contains 5: {}", arr.contains(&"5".to_string()));
+    println!("Reversed Elements:");
+    arr.reverse();
+    // Print elements line by line
+    for element in arr.iter() {
+        println!("{}", element);
+    }
 }
 
 fn main() {
-    let mut numbers = [1, 2, 3, 4, 5];
-    
-    // Call process_array
-    process_array(&mut numbers);
-    for i in 0..numbers.len() {
-        print!("{} ", numbers[i]);
-    }
-
+    let mut input_str_arr = String::new();
+    io::stdin().read_line(&mut input_str_arr).unwrap();
+    let mut arr: [String; 5] = input_str_arr.split(',').map(String::from).collect::<Vec<String>>().try_into().unwrap();
+    analyze_array(&mut arr);
 }
