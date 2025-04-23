3293e8fe5c686efca44b4f216fc8f764a3a72ac7
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-23 15:38:29 +0500

    Arrays Continued: Better Print

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-use std::io;
-use std::convert::TryInto;
-
-fn rev_arr(arr: &[i32]) -> [i32; 8] {
-    // Write your code below
-    let mut reversed: [i32; 8] = [0; 8];
-    for i in 0..arr.len() {
-        reversed[7-i] = arr[i];
-    }
-    reversed
-}
-
 fn main() {
-    let mut input_str_arr = String::new();
-    io::stdin().read_line(&mut input_str_arr).unwrap();
-    let input_str_arr = input_str_arr.trim();
-    let numbers: [i32; 8] = input_str_arr.split(',').map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>().try_into().unwrap();
-    let result = rev_arr(&numbers);
-    println!("The reversed array is: {:?}", result);
+    let numbers = [1, 2, 3, 4, 5];
+println!("{:?} ", numbers);
+let arr = [1.12345, 2.23456, 3.34567];
+println!("{:.2?}", arr);
 }
