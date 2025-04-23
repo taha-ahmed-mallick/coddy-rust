fbb28d8349ba72771b12d41b3c35179fe41415f6
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-23 15:35:04 +0500

    Arrays Continued: Recap - Reversed Array

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::io;
 use std::convert::TryInto;
 
-fn prod(arr: &[i32]) -> i32 {
+fn rev_arr(arr: &[i32]) -> [i32; 8] {
     // Write your code below
-    let mut result: i32 = 1;
-    for i in 0..arr.len() {
-        result*=arr[i];
+    let mut rev = [0; 8];
+    for (i, &val) in arr.iter().rev().enumerate() {
+        rev[i] = val;
     }
-    return result;
+   return rev;
 }
 
 fn main() {
@@ -15,6 +15,6 @@ fn main() {
     io::stdin().read_line(&mut input_str_arr).unwrap();
     let input_str_arr = input_str_arr.trim();
     let numbers: [i32; 8] = input_str_arr.split(',').map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>().try_into().unwrap();
-    let result = prod(&numbers);
-    println!("Product of array elements: {}", result);
+    let result = rev_arr(&numbers);
+    println!("The reversed array is: {:?}", result);
 }
