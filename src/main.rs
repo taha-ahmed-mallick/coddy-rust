7c6bec4ba6cb828781f66f4b047bcad71fd84236
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-25 13:08:37 +0500

    String Operations: String Conversion

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use std::io;
 
+fn convert_and_print(s: &str, n: f64, to_string: bool) {
+    // Write your code here
+    if to_string {
+        let n_str = n.to_string().replace(".", "").replace("-", "");
+        println!("Number: {}, Digits: {}", n, n_str.len());
+    } else {
+        let float: f64 = s.parse().unwrap();
+        let int: i32 = float as i32;
+        println!("String as number: {}", int);
+    }
+}
 
 fn main() {
-    let mut input_name = String::new();
-    let mut input_year = String::new();
-    io::stdin().read_line(&mut input_name).unwrap();
-    io::stdin().read_line(&mut input_year).unwrap();
-    let name = input_name.trim();
-    let year: i32 = input_year.trim().parse().unwrap();
+    let mut input_number_str = String::new();
+    let mut input_n = String::new();
 
-    // Write your code below
-    
-    
-    let secret_code = format!("⭐{}⭐-{}", name.chars().nth(0).unwrap().to_uppercase(), year.to_string().chars().rev().collect::<String>());
-    println!("{}", secret_code);
+    io::stdin().read_line(&mut input_number_str).unwrap();
+    io::stdin().read_line(&mut input_n).unwrap();
+
+    let n: f64 = input_n.trim().parse().unwrap();
+    let number_str = input_number_str.trim();
+
+    // Call convert_and_print with to_string = false
+    convert_and_print(number_str, n, false);
+
+    // Call convert_and_print with to_string = true
+    convert_and_print(number_str, n, true);
 }
